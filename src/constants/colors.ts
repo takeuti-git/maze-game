1409dd004286d0ac1d65016e14f364787019934e
takeuti-git <takeutiwastaken@gmail.com>
2025-12-26 18:20:17 +0900

    feat: wallOutlineにoffset追加

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -4,7 +4,7 @@ export const COLORS = {
     WALL_OUTLINE: "#00F",
     PLAYER: "#FF0",
     FOOD: "#AAA",
-    ONEWAY: "#FAA",
+    ONEWAY: "#FCC",
     ENEMY_1: "#F00",
     ENEMY_2: "#FAA",
     ENEMY_3: "#0FF",
--- src/ui/render.ts
+++ src/ui/render.ts
@@ -54,26 +54,70 @@ export class Renderer {
         this.ctx.closePath();
     }
 
+    /** WALLに対して、上下左右の壁を描く */
     private drawWall(coord: Coordinate, map: Map) {
         const tile = map.getTile(coord);
         if (tile !== TILE_TYPE.WALL) return;
 
         const { x, y } = this.coordToPixel(coord);
-        this.drawRect(x, y, this.tileSize, this.tileSize, COLORS.WALL);
+        // this.drawRect(x, y, this.tileSize, this.tileSize, COLORS.WALL);
 
         const neighbors = map.getAdjacentTiles(coord);
 
+        const coordOffset = 7;
+        const lengthOffset = coordOffset * 2;
+
         if (neighbors[Dir.UP] === TILE_TYPE.FLOOR) {
-            this.drawWallOutline(x, y, this.tileSize, this.outlineWidth);
+            const px = x + coordOffset;
+            const py = y + coordOffset;
+            const w = this.tileSize;
+            const h = this.outlineWidth;
+
+            if (neighbors[Dir.LEFT] === TILE_TYPE.WALL) {
+                this.drawWallOutline(px - lengthOffset - 1, py, w, h);
+            }
+            if (neighbors[Dir.RIGHT] === TILE_TYPE.WALL) {
+                this.drawWallOutline(px, py, w + 1, h);
+            }
         }
         if (neighbors[Dir.DOWN] === TILE_TYPE.FLOOR) {
-            this.drawWallOutline(x, y + this.tileSize - this.outlineWidth, this.tileSize, this.outlineWidth);
+            const px = x + coordOffset;
+            const py = y + this.tileSize - this.outlineWidth - coordOffset;
+            const w = this.tileSize;
+            const h = this.outlineWidth;
+
+            if (neighbors[Dir.LEFT] === TILE_TYPE.WALL) {
+                this.drawWallOutline(px - lengthOffset - 1, py, w, h);
+            }
+            if (neighbors[Dir.RIGHT] === TILE_TYPE.WALL) {
+                this.drawWallOutline(px, py, w + 1, h);
+            }
         }
         if (neighbors[Dir.LEFT] === TILE_TYPE.FLOOR) {
-            this.drawWallOutline(x, y, this.outlineWidth, this.tileSize);
+            const px = x + coordOffset;
+            const py = y + coordOffset;
+            const w = this.outlineWidth;
+            const h = this.tileSize;
+
+            if (neighbors[Dir.UP] === TILE_TYPE.WALL) {
+                this.drawWallOutline(px, py - lengthOffset, w, h);
+            }
+            if (neighbors[Dir.DOWN] === TILE_TYPE.WALL) {
+                this.drawWallOutline(px, py, w, h);
+            }
         }
         if (neighbors[Dir.RIGHT] === TILE_TYPE.FLOOR) {
-            this.drawWallOutline(x + this.tileSize - this.outlineWidth, y, this.outlineWidth, this.tileSize);
+            const px = x + this.tileSize - this.outlineWidth - coordOffset;
+            const py = y + coordOffset;
+            const w = this.outlineWidth;
+            const h = this.tileSize;
+
+            if (neighbors[Dir.UP] === TILE_TYPE.WALL) {
+                this.drawWallOutline(px, py - lengthOffset, w, h);
+            }
+            if (neighbors[Dir.DOWN] === TILE_TYPE.WALL) {
+                this.drawWallOutline(px, py, w, h);
+            }
         }
     }
 
@@ -81,36 +125,31 @@ export class Renderer {
         this.drawRect(x, y, w, h, COLORS.WALL_OUTLINE);
     }
 
-
     private drawFood(coord: Coordinate, foods: Foods) {
         if (!foods.has(coord)) return;
 
         const { cx, cy } = this.coordToCenterPixel(coord);
-        const size = 4;
-        this.drawRect(cx - (size / 2), cy - (size / 2), size, size, COLORS.FOOD);
+        const foodSize = 4; // 偶数なら中央揃えで表示される
+        this.drawRect(cx - (foodSize / 2), cy - (foodSize / 2), foodSize, foodSize, COLORS.FOOD);
     }
 
     private drawOneWay(coord: Coordinate) {
         const { x, y } = this.coordToPixel(coord);
-        this.drawRect(x, y + this.tileSize - this.outlineWidth, this.tileSize, this.outlineWidth, COLORS.ONEWAY);
+        this.drawRect(x - 8, y + 8, this.tileSize + 15, this.tileSize - 16, COLORS.ONEWAY);
     }
 
     public drawWorld(map: Map, foods: Foods) {
         this.drawRect(0, 0, this.canvas.width, this.canvas.height, COLORS.BACKGROUND);
-
-        if (DEBUG) {
-            this.ctx.strokeStyle = "#ccc";
-            this.ctx.font = "normal 8px system-ui";
-            this.ctx.textAlign = "left";
-        }
 
         for (let y = 0; y < map.height; y++) {
             for (let x = 0; x < map.width; x++) {
                 const coord = { x, y };
                 const tileType = map.getTile(coord);
                 if (DEBUG) {
-                    this.ctx.strokeText(`${x},${y}`, x * this.tileSize, y * this.tileSize + 10);
+                    // this.ctx.strokeText(`${x},${y}`, x * this.tileSize, y * this.tileSize + 10);
+                    this.drawRect(x * this.tileSize, y * this.tileSize, this.tileSize, this.tileSize, (x + y) % 2 === 0 ? "#333" : "#555");
                 }
+
 
                 if (map.isWall(coord)) this.drawWall(coord, map);
                 else if (foods.has(coord)) this.drawFood(coord, foods);
@@ -153,8 +192,9 @@ export class Renderer {
 
     public drawTargetPosition(coord: Coordinate, color: string) {
         const { cx, cy } = this.coordToCenterPixel(coord);
-        const offset = 10;
-        const wh = this.tileSize - offset;
-        this.drawRect(cx - (offset / 2), cy - (offset / 2), wh, wh, color);
+        const boxSize = 10;
+        const wh = this.tileSize - boxSize;
+        this.drawRect(cx - (boxSize / 2) - 1, cy - (boxSize / 2) - 1, wh + 2, wh + 2, "#FFF");
+        this.drawRect(cx - (boxSize / 2), cy - (boxSize / 2), wh, wh, color);
     }
 }
