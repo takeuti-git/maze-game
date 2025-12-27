a2ac8844c2767430414f092f193b8c137132dc40
takeuti-git <takeutiwastaken@gmail.com>
2025-12-27 17:05:31 +0900

    etc: コメント追加

--- src/game/coord.ts
+++ src/game/coord.ts
@@ -12,6 +12,7 @@ export function calcCoordFromVector(coord: Coordinate, dir: Vector): Coordinate 
     return { x: coord.x + dir.vx, y: coord.y + dir.vy };
 }
 
+/** 座標Cを中心とした時の座標Pに対するベクトルを180度回転させた座標を返す */
 export function calcOppositeCoordinate(C: Coordinate, P: Coordinate): Coordinate {
     return { x: 2 * C.x - P.x, y: 2 * C.y - P.y };
 }
